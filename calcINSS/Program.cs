e3cf29122a91687d5fd8da55b610420bcf803611
Milton Carlos Katoo <mckatoo@gmail.com>
2018-08-30 22:15:54 -0300

    first commit

---
+++ calcINSS/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace calcINSS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double salarioBruto, salarioLiquido, INSS, IRRF, deducaoIR;
+
+            Console.WriteLine("Nome:");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Informe o salário bruto:");
+            salarioBruto = float.Parse(Console.ReadLine());
+
+            // CALC INSS
+            INSS = (salarioBruto * 0.11);
+            if (salarioBruto < 1693.73)
+                INSS = salarioBruto * 0.08;
+            if ((salarioBruto > 1693.72) && (salarioBruto < 2822.91))
+                INSS = (salarioBruto * 0.09);
+
+            // CALC IRRF
+            IRRF = 0;
+            deducaoIR = 0;
+            if ((salarioBruto > 1903.98) && (salarioBruto < 2826.66))
+                IRRF = salarioBruto * 0.075 - 142.80;
+            if ((salarioBruto > 2826.65) && (salarioBruto < 3751.06))
+                IRRF = salarioBruto * 0.15 - 354.80;
+            if ((salarioBruto > 3751.05) && (salarioBruto < 4664.69))
+                IRRF = salarioBruto * 0.225 - 636.13;
+            if (salarioBruto > 4664.68)
+                IRRF = salarioBruto * 0.275 - 869.36;
+
+            // CALC GERAL
+            salarioLiquido = salarioBruto - INSS - IRRF;
+            Console.WriteLine($"INSS: {INSS}");
+            Console.WriteLine($"Imposto de renda: {IRRF}");
+            Console.WriteLine($"Salário líquido é: {salarioLiquido}");
+        }
+    }
+}
---
+++ calcNotaFreq/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace calcNotaFreq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o nome do aluno: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Informe a quantidade de aulas dadas: ");
+            int aulasDadas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a quantidade de falta: ");
+            int falta = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a nota da NP1: ");
+            float np1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a nota da NP2: ");
+            float np2 = float.Parse(Console.ReadLine());
+            float mediaExame = 0;
+            float media = (np1 + np2) / 2;
+            bool reprovouPorFalta = false;
+            bool ficouExame = false;
+
+            if ((falta * 100) / aulasDadas > 25)
+            {
+                reprovouPorFalta = true;
+                Console.WriteLine($"O aluno {nome} reprovou por falta.");
+            }
+            // if ((media > 7) && (reprovouPorFalta == false))
+            // {
+            //     Console.WriteLine("Passou!");
+            // }
+            if ((media < 7) && (reprovouPorFalta == false))
+            {
+                Console.WriteLine($"O aluno {nome} ficou de exame. Informe a nota do exame");
+                float notaExame = float.Parse(Console.ReadLine());
+                mediaExame = (np1 + np2 + notaExame) / 3;
+                if (mediaExame < 5)
+                    ficouExame = true;
+            }
+            if ((ficouExame == false) && (reprovouPorFalta == false))
+            {
+                Console.WriteLine("Passou mardito.");
+            }
+            else
+            {
+                Console.WriteLine($"Reprovado com media de exame {Math.Round(mediaExame,2)}");
+            }
+        }
+    }
+}
